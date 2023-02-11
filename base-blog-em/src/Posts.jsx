d19569e3efd61b4e0981b39dc0d984262fb20d49
8dong <hyde981114@gmail.com>
2023-02-11 23:53:19 +0900

    feat: prefetchQuery를 통해 pre-fetching 기능 구현

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useState, useEffect } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
@@ -14,6 +14,15 @@ async function fetchPosts(pageNum) {
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
+
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(['posts', nextPage], () => fetchPosts(nextPage));
+    }
+  }, [currentPage, queryClient]);
 
   // replace with useQuery
   const { data, isLoading, isError } = useQuery(['posts', currentPage], () =>
